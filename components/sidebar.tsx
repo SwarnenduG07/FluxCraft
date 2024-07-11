2137973254f1d775c7e69f980aca18bb32a1d38b
Sunny <swarnendughosh05@gmail.com>
2024-07-11 12:57:36 +0530

    side bar components added

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -4,10 +4,57 @@ import { Montserrat } from "next/font/google"
 import Image from "next/image"
 import Link from "next/link"
 import { cn } from "@/lib/utils"
+import { Code, Code2Icon, CodeIcon, CodeSquare, CodeXmlIcon, ImageIcon, LayoutDashboard, LucideMusic2, MessageSquare, Music, Music2, Music3Icon, Music4Icon, Settings, VideoIcon } from "lucide-react"
 
 const montserrat = Montserrat({
    weight: "600", subsets: ["latin"]
 })
+
+const routes = [
+   {
+      lable: "Dashboard",
+      icon: LayoutDashboard,
+      href: "/dashboard",
+      color: "text-sky-500"
+   },
+   {
+      lable: "Conversation",
+      icon: MessageSquare,
+      href: "/conversation",
+      color: "text-violet-500"
+   },
+   {
+      lable: "Image Genaration",
+      icon: ImageIcon,
+      href: "/image",
+      color: "text-pink-700"
+   },
+   {
+      lable: "Video Genaration",
+      icon: VideoIcon,
+      href: "/video",
+      color: "text-sky-500"
+   },
+   {
+      lable: "Music Genaration",
+      icon: Music4Icon,
+      href: "/music",
+      color: "text-emerald-500"
+   },
+   {
+      lable: "Code Genaration",
+      icon: Code,
+      href: "/code",
+      color: "text-green-700"
+   },
+   {
+      lable: "Setting",
+      icon: Settings,
+      href: "/setting",
+      color: "text-sky-500"
+   }
+]
+
 
 const SideBar = () =>{
     return (
@@ -21,6 +68,20 @@ const SideBar = () =>{
                       FluxCraft
                    </h1>
                 </Link>
+                <div className="space-y-1">
+                  {routes.map((route) => (
+                     <Link 
+                     href={route.href}
+                     key={route.href}
+                     className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-blue-800 rounded-lg"
+                     >
+                     <div className="flex items-center flex-1">
+                        <route.icon className={cn ("h-5 w-5 mr-3", route.color)}/>
+                        {route.lable}
+                     </div>
+                     </Link>
+                  ))}
+                </div>
              </div>
         </div>
     )
