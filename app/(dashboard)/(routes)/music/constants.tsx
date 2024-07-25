3760e960b2088c3336dc919414ca2b9d24824d1f
Sunny <swarnendughosh05@gmail.com>
2024-07-26 00:23:55 +0530

    constand updated for image gen

--- app/(dashboard)/(routes)/music/constants.tsx
+++ app/(dashboard)/(routes)/music/constants.tsx
@@ -1,7 +1,43 @@
+import { Label } from "@radix-ui/react-dropdown-menu";
 import * as z from "zod";
 
 export const formSchema = z.object({
      prompt: z.string().min(1, {
         message: "Music Prompt is required"
      }),
-}) 
+     amount: z.string().min(1),
+     resolution: z.string().min(1)
+});
+export const authOptions = [
+   {
+      value: "1",
+      Label: "1 Photo"
+   },
+   {
+      value: "2",
+      Label:"2 Photo"
+   },
+   {
+      value: "3",
+      lable:"3 Photo"
+   },
+   {
+      value: "4",
+      lable: "4 Photo"
+   }
+]
+
+ export const resolutionOptions =[
+    {
+       value : "256x256",
+       lable:"256x256"
+    },
+    {
+      value: "512x512",
+      Label: "512x512",
+    },
+    {
+      value: "1024x1024",
+      lable:"1024x1024"
+    }
+ ]
