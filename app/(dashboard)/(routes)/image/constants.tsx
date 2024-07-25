96e262cd8701bd7fa7d48406dace0ec8496b306a
Sunny <swarnendughosh05@gmail.com>
2024-07-25 20:44:43 +0530

    nug fixed

--- app/(dashboard)/(routes)/image/constants.tsx
+++ app/(dashboard)/(routes)/image/constants.tsx
@@ -2,6 +2,8 @@ import * as z from "zod";
 
 export const formSchema = z.object({
      prompt: z.string().min(1, {
-        message: "Prompt is required"
+        message: "Image Prompt is required"
      }),
-}) 
+     amount: z.string().min(1),
+     resolution: z.string().min(1),
+})
