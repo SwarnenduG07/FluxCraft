16a82a912eaba472394fc8eb92e76093badde70a
Sunny <swarnendughosh05@gmail.com>
2024-07-27 02:01:01 +0530

    img gen api completed

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -2,7 +2,8 @@
 import axios from "axios"
 import * as z from "zod"
 import { Headings } from "@/components/headings"
-import { Image, MessageSquare } from "lucide-react"
+import { Download, ImageIcon } from "lucide-react"
+import Image from 'next/image'
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { amountOptions, formSchema, resolutionOptions } from "./constants"
@@ -14,14 +15,13 @@ import { useState } from "react"
 import { Empty } from "@/components/empty"
 import { Loader } from "@/components/loader"
 import { cn } from "@/lib/utils"
-import { UserAvater } from "@/components/user-avater"
-import { BotAvater } from "@/components/bot-avater"
 import { useProModel } from "@/app/hooks/use-pro-model"
 import toast from "react-hot-toast"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { Card, CardFooter } from "@/components/ui/card"
 
 const Music = () => {
-    const proModel = useProModel()
+    const proModel = useProModel();
     const router = useRouter();
     const [images, setImages] = useState<string[]>([]);
     const form = useForm<z.infer<typeof formSchema>>({
@@ -29,7 +29,7 @@ const Music = () => {
         defaultValues: {
             prompt: "",
             amount: "1",
-            resolution:"512x512"
+            resolution: "512x512"
         }
     });
 
@@ -37,20 +37,19 @@ const Music = () => {
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         try {
-            setImages([])
-            const response = await axios.post("/api/image",values);
+            setImages([]);
+            const response = await axios.post("/api/image", values);
+            const base64Img = await response.data.image;
+            const imageUrl = `data:image/png;base64,${base64Img}`;
+            setImages([imageUrl]);  // Assuming you're getting a single image
 
-         const urls = response.data.map(( image: {url : string}) =>  image.url)
-         setImages(urls)
-            
-           form.reset();
+            form.reset();
         } catch (e: any) {
-           if(e?.response?.status === 403)  {
+            if (e?.response?.status === 403) {
                 proModel.onOpen();
-           } else {
-              toast.error("We don't have API cradit's left.Try after some time")
-           }
-           
+            } else {
+                toast.error("Something went wrong");
+            }
         } finally {
             router.refresh();
         }
@@ -58,123 +57,142 @@ const Music = () => {
 
     return (
         <div>
-            <Headings 
+            <Headings
                 title="Image"
-                description="Our Most Advanced Image Generation Model "
-                icon={Image}
+                description="Our Most Advanced Image Generation Model"
+                icon={ImageIcon}
                 iconColor="text-pink-500"
                 bgColor="bg-pink-500/10"
             />
             <div className="px-4 lg:px-8">
                 <div>
                     <Form {...form}>
-                        <form 
-                            onSubmit={form.handleSubmit(onSubmit)} 
+                        <form
+                            onSubmit={form.handleSubmit(onSubmit)}
                             className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
                         >
-                            <FormField 
+                            <FormField
                                 name="prompt"
                                 render={({ field }) => (
                                     <FormItem className="col-span-12 lg:col-span-10">
                                         <FormControl className="p-0 m-0">
-                                            <Input 
-                                                {...field} // Bind field properties to the input
+                                            <Input
+                                                {...field}
                                                 className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
                                                 disabled={isLoading}
-                                                placeholder="Generate A image of a Man setting on a Horse ⇒"
+                                                placeholder="Generate an image of a man sitting on a horse ⇒"
                                             />
                                         </FormControl>
                                     </FormItem>
                                 )}
                             />
-                         <FormField 
-                         control={form.control}
-                          name="amount"
-                          render={( { field }) => (
-                            <FormItem className="col-span-12 lg:col-span-6">
-                               <Select 
-                               disabled={isLoading}
-                               onValueChange={field.onChange}
-                               value={field.value}
-                               defaultValue={field.value}
-                               >
-                             <FormControl>
-                                <SelectTrigger>
-                                     <SelectValue defaultValue={field.value}/>
-                                   </SelectTrigger>
-                                 </FormControl>
-                                 <SelectContent>
-                                    {amountOptions.map((option) => (
-                                        <SelectItem 
-                                        key={option.value}
-                                        value={option.value}
+                            <FormField
+                                control={form.control}
+                                name="amount"
+                                render={({ field }) => (
+                                    <FormItem className="col-span-12 lg:col-span-6">
+                                        <Select
+                                            disabled={isLoading}
+                                            onValueChange={field.onChange}
+                                            value={field.value}
+                                            defaultValue={field.value}
                                         >
-                                         {option.lable}
-                                        </SelectItem>
-                                    ))}
-                                 </SelectContent>
-                              </Select>
-                            </FormItem> 
-                          )}
-                         
-                         />
-                         <FormField 
-                         control={form.control}
-                          name="resolution"
-                          render={( { field }) => (
-                            <FormItem className="col-span-12 lg:col-span-6">
-                               <Select 
-                               disabled={isLoading}
-                               onValueChange={field.onChange}
-                               value={field.value}
-                               defaultValue={field.value}
-                               >
-                             <FormControl>
-                                <SelectTrigger>
-                                     <SelectValue defaultValue={field.value}/>
-                                   </SelectTrigger>
-                                 </FormControl>
-                                 <SelectContent>
-                                    {resolutionOptions.map((option) => (
-                                        <SelectItem 
-                                        value={option.value}
-                                        key={option.value}
+                                            <FormControl>
+                                                <SelectTrigger>
+                                                    <SelectValue defaultValue={field.value} />
+                                                </SelectTrigger>
+                                            </FormControl>
+                                            <SelectContent>
+                                                {amountOptions.map((option) => (
+                                                    <SelectItem
+                                                        key={option.value}
+                                                        value={option.value}
+                                                    >
+                                                        {option.lable}
+                                                    </SelectItem>
+                                                ))}
+                                            </SelectContent>
+                                        </Select>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="resolution"
+                                render={({ field }) => (
+                                    <FormItem className="col-span-12 lg:col-span-6">
+                                        <Select
+                                            disabled={isLoading}
+                                            onValueChange={field.onChange}
+                                            value={field.value}
+                                            defaultValue={field.value}
                                         >
-                                         {option.lable}
-                                        </SelectItem>
-                                    ))}
-                                 </SelectContent>
-                              </Select>
-                            </FormItem> 
-                          )}
-                         
-                         />
-
-                           <Button className="col-span-3 lg:col-span-2 w-full" 
-                           disabled={isLoading}
-                           variant="premium"
-                           >
-                                 Genarate
-                           </Button>
+                                            <FormControl>
+                                                <SelectTrigger>
+                                                    <SelectValue defaultValue={field.value} />
+                                                </SelectTrigger>
+                                            </FormControl>
+                                            <SelectContent>
+                                                {resolutionOptions.map((option) => (
+                                                    <SelectItem
+                                                        value={option.value}
+                                                        key={option.value}
+                                                    >
+                                                        {option.lable}
+                                                    </SelectItem>
+                                                ))}
+                                            </SelectContent>
+                                        </Select>
+                                    </FormItem>
+                                )}
+                            />
+                            <Button className="col-span-3 lg:col-span-2 w-full"
+                                disabled={isLoading}
+                                variant="premium"
+                            >
+                                Generate
+                            </Button>
                         </form>
                     </Form>
                 </div>
                 <div className="space-y-4 mt-4">
                     {isLoading && (
-                     <div className="p-20">
-                       <Loader />
+                        <div className="p-20">
+                            <Loader />
                         </div>
                     )}
-                    {images.length ===  0 && !isLoading && (
+                    {images.length === 0 && !isLoading && (
                         <Empty label={"No Image generated"} />
                     )}
-                  <div>
-                      Image will be generated
-                  </div>
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-8">
+                        {images.map((src) => (
+                            <Card key={src} className="rounded-lg overflow-hidden">
+                                <div className="relative aspect-square">
+                                    <Image
+                                        alt="Generated Image"
+                                        fill
+                                        src={src}
+                                        layout="fill"
+                                        objectFit="cover"
+                                    />
+                                </div>
+                                <CardFooter className="p-2">
+                                    <Button
+                                        onClick={() => window.open(src)}
+                                        variant="secondary"
+                                        className="w-full">
+                                          
+                                        <Download className="h-4 w-4 mr-2" />
+                                        Download
+                                    </Button>
+                                </CardFooter>
+                            </Card>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
     )
 }
 
-export default Music
+export default Music;
---
+++ app/api/image/route.ts
@@ -0,0 +1,61 @@
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+require("dotenv").config();
+import { checkApiLimit, increseApiLimit } from "@/lib/api-limit";
+import { checkSubscription } from "@/lib/subscription";
+
+export async function POST(req: Request, res: Response) {
+    try {
+        const { userId } = auth();
+        const body = await req.json();
+        const { prompt } = body;
+
+        if (!userId) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        if (!prompt) {
+            return new NextResponse("Prompt is required", { status: 400 });
+        }
+
+        const free = await checkApiLimit();
+        const isPro = await checkSubscription();
+
+        if (!free && !isPro) {
+            return new NextResponse("Free trial has expired", { status: 403 });
+        }
+
+        const response = await fetch(
+            "https://api-inference.huggingface.co/models/SG161222/RealVisXL_V4.0",
+            {
+                headers: {
+                    Authorization: `Bearer ${process.env.HF_API_KEY}`,
+                    "Content-Type": "application/json",
+                },
+                method: "POST",
+                body: JSON.stringify({ inputs: prompt }),
+            }
+        );
+
+        if (!response.ok) {
+            const errorDetails = await response.json();
+            console.error(`Hugging Face API error: ${response.statusText}`, errorDetails);
+            return new NextResponse("Error generating image", { status: response.status });
+        }
+
+        const blob = await response.blob();
+        const arrayBuffer = await blob.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+        const base64Image = buffer.toString('base64'); 
+    
+        
+
+        return NextResponse.json({ image: base64Image }, {
+            headers: { "Content-Type": "application/json" },
+        });
+    } catch (e: any) {
+        console.error("Image generation error:", e);
+        return new NextResponse("Internal Server Error", { status: 500 });
+    }
+}
